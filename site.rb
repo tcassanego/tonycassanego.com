92d469326a61d37a5b5456bf2699e217bc1d365a
Tony <tonyc@cs.stanford.edu>
2012-10-29 16:51:57 -0700

    Add Sinatra AssetPack

--- site.rb
+++ site.rb
@@ -1,11 +1,22 @@
 require 'sinatra'
 require 'haml'
-
-set :haml, :format => :html5
+require 'sinatra/assetpack'
 
 configure :production do
   require 'newrelic_rpm'
 end
+
+set :haml, :format => :html5
+
+Sinatra::Application.register Sinatra::AssetPack
+
+assets {
+  js :main, ['/js/main.js']
+  css :main, ['/css/main.css']
+
+  js_compression  :jsmin
+  css_compression :simple
+}
 
 get '/' do
   haml :index
